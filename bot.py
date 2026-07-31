18322f7271c7399fabdc68168f630a6af1f9936f
yomibiznes-dotcom <yomi.biznes@gmail.com>
2026-07-31 17:03:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,40 @@
 import discord
-import os
+from discord import app_commands
 from discord.ext import commands
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = "TU_WKLEJ_TOKEN"
 
 intents = discord.Intents.default()
-intents.message_content = True
-
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+class MyClient(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="!", intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        await self.tree.sync()
+
+bot = MyClient()
 
 @bot.event
 async def on_ready():
-    print(f"Bot działa jako {bot.user}")
+    print(f"Zalogowano jako {bot.user}")
 
-@bot.command()
-async def hej(ctx):
-    await ctx.send("Cześć!")
+@bot.tree.command(name="leak", description="Wyślij wiadomość z obrazem i plikiem")
+@app_commands.describe(
+    tekst="Co bot ma napisać",
+    obraz_link="Link do zdjęcia / filmu",
+    plik="Plik do wysłania"
+)
+async def leak(interaction: discord.Interaction, tekst: str, obraz_link: str, plik: discord.Attachment):
+
+    embed = discord.Embed(description=tekst, color=discord.Color.red())
+    embed.set_image(url=obraz_link)
+
+    await interaction.response.send_message(
+        embed=embed,
+        file=await plik.to_file()
+    )
 
 bot.run(TOKEN)
